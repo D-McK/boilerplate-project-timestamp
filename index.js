f53537efbefa2e6bf91ae4d1cbc362ad0fc8da3d
Dan McKeown <danielmckeown555@hotmail.co.uk>
2023-08-25 16:07:58 +0100

    timestamp microservice solution

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:date?", (req, res) => {
+  res.json({utc: new Date(`${req.params.date}`).toString(), unix: new Date(`${req.params.date}`).getTime()})
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
